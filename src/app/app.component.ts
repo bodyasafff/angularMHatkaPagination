1002ebea16442637be2698d7c81879ab9d769dad
Богдан Сафнюк <bodyasafff@gmail.com>
2020-09-11 16:23:25 +0300

    testApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'AngularMHatkaPagination';
+
+  constructor(private http: HttpClient){
+  }
+  
+  ngOnInit(){
+    this.http.get('http://tekstil-rivne.com.ua/Product/GetProducts?numPage=1&idgroup=2&country=&material=&brend=&typeCloth=&filler=&special=').subscribe(res => console.log(res));
+  }
 }
